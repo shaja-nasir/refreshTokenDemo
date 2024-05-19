e347d60f0efeeb83c874337e78cabd35cfbcb560
Shaja Nasir <Shajanasir@gmail.com>
2024-05-19 17:12:39 +0300

    Inital Setup

---
+++ src/main/java/com/spring/qa/RefreshDemo/RefreshDemoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.qa.RefreshDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefreshDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefreshDemoApplication.class, args);
+	}
+
+}
